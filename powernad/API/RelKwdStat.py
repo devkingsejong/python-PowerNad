a72de703a24190b5129e6c6511a0b0992ecfb168
cuhong <hongcoilhouse@gmail.com>
2019-01-13 12:09:35 +0900

    Update RelKwdStat.py
    해당 메서드는 business ID, ncc business channel ID, keyword hints, season, event ID 중 하나만 입력되면 됩니다. 따라서 siteId 부분도 siteId = None으로 기본값을 주도록 수정했습니다. 아마 다른 메서드도 그럴 것 같은데 아직 확인은 못해봤어요. 제 노가다를 줄여주셔서 감사합니다^^

--- powernad/API/RelKwdStat.py
+++ powernad/API/RelKwdStat.py
@@ -9,7 +9,7 @@ class RelKwdStat:
     def __init__(self, base_url: str, api_key: str, secret_key: str, customer_id: int):
         self.r = RestApi(base_url, api_key, secret_key, customer_id)
 
-    def get_rel_kwd_stat_list(self, siteId: str, biztpId: int = None, hintKeywords: str = None, event: int = None,
+    def get_rel_kwd_stat_list(self, siteId: str = None, biztpId: int = None, hintKeywords: str = None, event: int = None,
                               month: int = None, showDetail: int = None) -> RelKwdStatObjectList:
 
         query = {'siteId': siteId, 'biztpId': biztpId, 'hintKeywords': hintKeywords,
