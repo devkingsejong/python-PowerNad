eadbba65bdb2f188929493413866bc95f15b8dba
devkingsejong <devkingsejong@gmail.com>
2020-05-28 13:07:19 +0900

    DataPrest관련 Typo를 수정합니다.

--- powernad/API/Stat.py
+++ powernad/API/Stat.py
@@ -11,19 +11,19 @@ class Stat:
     def __init__(self, base_url: str, api_key: str, secret_key: str, customer_id: int):
         self.r = RestApi(base_url, api_key, secret_key, customer_id)
 
-    def get_stat_by_id(self, id: str, fields: str, timeRange: str, dataPreset: str = None, timeIncrement: str = None,
+    def get_stat_by_id(self, id: str, fields: str, timeRange: str, datePreset: str = None, timeIncrement: str = None,
                                                                                     breakdown: str = None) -> StatObject:
-        query = {'id': id, 'fields': fields, 'timeRange': timeRange, 'dataPreset': dataPreset,
+        query = {'id': id, 'fields': fields, 'timeRange': timeRange, 'datePreset': datePreset,
                      'timeIncrement': timeIncrement, 'breakdown': breakdown}
         result = self.r.get('/stats', query)
         result = StatObject(result)
 
         return result
         
-    def get_stat_by_ids(self, ids: StatIdList, fields: str, timeRange: str, dataPreset: str= None,
+    def get_stat_by_ids(self, ids: StatIdList, fields: str, timeRange: str, datePreset: str= None,
                                                         timeIncrement: str = None, breakdown: str = None) -> StatObject:
 
-        query = {'ids': ids, 'fields': fields, 'timeRange': timeRange, 'dataPreset': dataPreset,
+        query = {'ids': ids, 'fields': fields, 'timeRange': timeRange, 'dataPreset': datePreset,
                      'timeIncrement': timeIncrement, 'breakdown': breakdown}
         result = self.r.get('/stats', query)
         result = StatObject(result)
